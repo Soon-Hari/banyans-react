93283f6a4c45c7317f9b6783d61012fdaf76b1d2
Jermaine Bruno <jbruno7@hawaii.edu>
2025-10-16 14:14:48 -1000

    yes

--- my-app/src/components/BottomFooter.tsx
+++ my-app/src/components/BottomFooter.tsx
@@ -1,5 +1,5 @@
+import React from "react";
 'use client';
-import React from 'react';
 import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 
 export default function BottomFooter() {
--- my-app/src/components/TopMenu.tsx
+++ my-app/src/components/TopMenu.tsx
@@ -1,5 +1,6 @@
+
+import React from "react";
 'use client';
-import React from 'react';
 import { Navbar, Nav, Container, Image } from 'react-bootstrap';
 
 export default function TopMenu() {
